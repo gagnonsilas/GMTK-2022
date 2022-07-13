0e8cf9f129a55ad6f283aa9ecd89bc18d937dd3a
gagnonsilas <gagnon.silas@gmail.com>
2022-07-13 14:38:36 -0700

    refactor: rename to use snake case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(TestPlugin)
+        .run();
 }
+
+
+fn say_hi() {
+    println!("hi");
+}
+
+pub struct TestPlugin;
+
+impl Plugin for TestPlugin {
+    fn build(&self, app: &mut App) {
+        println!("wow this is pretty cool");
+
+        app.add_system(say_hi);
+    }
+}
+
+
